bcb3b28b79452f957cefce74ccf88a484805a51e
Mia Mahmudunnabi <mia.mahmudunnabi@petronas.com>
2023-05-08 16:11:44 +0600

    Autofact Added

---
+++ FluentApi/AutofacModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using FluentApi.Data;
+using FluentApi.Model;
+using FluentApi.Repository;
+using FluentApi.Service;
+using FluentApi.Validation;
+using FluentValidation;
+
+namespace FluentApi
+{
+    public class AutofacModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StudentValidator>().As<IValidator<Student>>().InstancePerDependency();
+            builder.RegisterType<StudentService>().As<IStudentService>().InstancePerLifetimeScope();
+            builder.RegisterType<StudentRepository>().As<IStudentRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<DapperContext>().AsSelf().SingleInstance();
+        }
+    }
+}
--- FluentApi/Program.cs
+++ FluentApi/Program.cs
@@ -1,3 +1,6 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FluentApi;
 using FluentApi.Data;
 using FluentApi.Model;
 using FluentApi.Repository;
@@ -9,6 +12,16 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(container =>
+{
+    //container.RegisterType<StudentValidator>().As<IValidator<Student>>().InstancePerDependency();
+    //container.RegisterType<StudentService>().As<IStudentService>().InstancePerLifetimeScope();
+    //container.RegisterType<StudentRepository>().As<IStudentRepository>().InstancePerLifetimeScope();
+    //container.RegisterType<DapperContext>().AsSelf().SingleInstance();
+    container.RegisterModule(new AutofacModule());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -16,10 +29,10 @@ builder.Services.AddSwaggerGen(c =>
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IValidator<Student>, StudentValidator>();
-builder.Services.AddScoped<IStudentService, StudentService>();
-builder.Services.AddSingleton<DapperContext>();
-builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+//builder.Services.AddScoped<IValidator<Student>, StudentValidator>();
+//builder.Services.AddScoped<IStudentService, StudentService>();
+//builder.Services.AddSingleton<DapperContext>();
+//builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
